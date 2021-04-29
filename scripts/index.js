54c982b94a205a22c358a053804138580daebb2c
vhaldemario <vhaldemario@gmail.com>
2021-04-29 21:51:01 +0500

    refactor to template tag

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,17 @@
 // что, с чем, когда
 
-// ввод в инпут
-// кнопка
-// добавление новой строки
-
 const button = document.querySelector('.add__button');
 const input = document.querySelector('#add-todo');
 const tasksContainer = document.querySelector('.tasks__list');
+const taskTemplate = document.querySelector('#task-template');
+
+console.log(taskTemplate);
 
 button.addEventListener('click', function() {
     const taskValue = input.value;
+    const newTask = taskTemplate.content.querySelector('.tasks__item').cloneNode(true);
 
-    tasksContainer.insertAdjacentHTML('beforeend', `<li class="tasks__item">
-        <label class="field tasks__label">
-          <input class="visually-hidden tasks__input" type="checkbox"> 
-          <span class="tasks__indicator"></span>
-          <span class="tasks__text">${taskValue}</span>
-          <span class="tasks__trash"></span>
-        </label>
-      </li>`);
+    newTask.querySelector('.tasks__text').textContent = taskValue;
+
+    tasksContainer.append(newTask);
 });
