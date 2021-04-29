b99438f205a77c8bbc56624709db1e91179adb59
vhaldemario <vhaldemario@gmail.com>
2021-04-29 22:38:27 +0500

    feature: calc count

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const button = document.querySelector('.add__button');
 const input = document.querySelector('#add-todo');
 const tasksContainer = document.querySelector('.tasks__list');
 const taskTemplate = document.querySelector('#task-template');
+const countIndicator = document.querySelector('.footer__count');
 
 const initialData = [
     'Научиться динамически добавлять контент',
@@ -13,27 +14,36 @@ const initialData = [
 ];
 
 function createTodo(itemData) {
+    function handleRemoveTodo(e) {
+        e.target.closest('.tasks__item').remove();
+        recalculateCount();
+    }
+
     const newTask = taskTemplate.content.querySelector('.tasks__item').cloneNode(true);
     const cardText = newTask.querySelector('.tasks__text');
     const cardRemoveButton = newTask.querySelector('.tasks__trash');
 
     cardText.textContent = itemData;
 
-    cardRemoveButton.addEventListener('click', function(e) {
-        console.log('Click!');
-        e.target.closest('.tasks__item').remove();
-    });
+    cardRemoveButton.addEventListener('click', handleRemoveTodo);
 
     return newTask;
+}
+
+function recalculateCount() {
+    const count = tasksContainer.querySelectorAll('.tasks__item').length;
+    countIndicator.textContent = count;
 }
 
 initialData.forEach(function(currentItem) {
     const newCard = createTodo(currentItem);
     tasksContainer.append(newCard);
+    recalculateCount();
 });
 
 button.addEventListener('click', function() {
     const taskValue = input.value;
 
     tasksContainer.append(createTodo(taskValue));
+    recalculateCount();
 });
